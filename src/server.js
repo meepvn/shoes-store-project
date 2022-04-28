b186ce07673813fc59a9702b8a134074c4e03e57
unknown <hanhcu7777@gmail.com>
2022-04-28 15:08:58 +0700

    cors

--- src/server.js
+++ src/server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const initAPIRoute = require('./routes/api');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 const port = process.env.PORT || 3100;
 
-
+app.use(cors({
+    origin:"*"
+}))
 app.use(express.static('./src/public'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
