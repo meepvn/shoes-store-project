3f815654d72687cdc035cb68584fea01a4e5e7e3
unknown <hanhcu7777@gmail.com>
2022-04-29 17:14:52 +0700

    asd

--- src/controllers/CustomerController.js
+++ src/controllers/CustomerController.js
@@ -33,11 +33,12 @@ class CustomerController{
             });
         }
         const {name,phone,address,email} = req.body;
-        await pool.execute(`insert into khachhang (TenKH,SDT,DiaChi,Email) 
+        const [result] = await pool.execute(`insert into khachhang (TenKH,SDT,DiaChi,Email) 
         values (?,?,?,?) `,[name,phone,address,email]);
         res.status(200).json({
             message:'Success',
             insertedData:{
+                id:result.insertId,
                 name,
                 phone,
                 address,
@@ -48,9 +49,10 @@ class CustomerController{
     
     async deleteCustomer(req,res){
         if(!req.params.id){
-            return res.json({
+            res.json({
                 message:'failed'
-            })
+            });
+            return;
         }
         await pool.execute(`delete from khachhang where MaKH = ?`,[req.params.id]);
         res.status(200).json({
