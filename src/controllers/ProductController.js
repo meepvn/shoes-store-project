86cb971ad58a2257c6c7ed87c7523ade697f3526
unknown <hanhcu7777@gmail.com>
2022-05-02 13:54:05 +0700

    add product controller

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -15,22 +15,22 @@ class ProductController{
                 message:"Missing required parameter(s)"
             });
         }
-        const {name,instock,price} = req.body;
+        const {name,instock,price,producer,category} = req.body;
         await pool.execute(`update sanpham set TenSP = ?, 
-        SoLuong = ?, DonGia = ? where MaSP = ? `,[name,instock,price,req.params.id]);
+        SoLuong = ?, DonGia = ?, Loai = ?, HangSX = ? where MaSP = ? `,[name,instock,price,category,producer,req.params.id]);
         res.status(200).json({
             message:"ok, updated"
         });
     }
     async insertProduct(req,res){
-        if(!req.body.name || !req.body.instock || !req.body.price ) {
+        if(!req.body.name || !req.body.instock || !req.body.price || !req.body.producer || !req.body.category) {
             return res.json({
                 message:"Missing required parameter(s)"
             });
         }
-        const {name,instock,price} = req.body;
-        await pool.execute(`insert into sanpham (TenSP,SoLuong,DonGia) 
-        values (?,?,?) `,[name,instock,price]);
+        const {name,instock,price, producer,category} = req.body;
+        await pool.execute(`insert into sanpham (TenSP,Loai,HangSX,DonGia,SoLuong) 
+        values (?,?,?,?,?) `,[name,category,producer,price,instock]);
         res.status(200).json({
             message:'Success',
         })
