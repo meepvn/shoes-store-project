8c961aa5b8087cb5b06c16033ade664774785b97
unknown <hanhcu7777@gmail.com>
2022-05-09 15:40:11 +0700

    asd

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const customerController = require("../controllers/CustomerController");
 
 router.get("/find/:name", customerController.getCustomerByNameRelatively);
-router.post("/create", customerController.insertCustomer);
 router.delete("/:id", customerController.deleteCustomer);
-router.put("/:id/edit", customerController.updateCustomerByID);
+router.put("/:id/", customerController.updateCustomerByID);
 router.get("/:id", customerController.getCustomerByID);
+router.post("/", customerController.insertCustomer);
 router.get("/", customerController.getAllCustomer);
 
 module.exports = router;
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const productController = require('../controllers/ProductController');
+const productController = require("../controllers/ProductController");
 
-router.post('/create',productController.insertProduct)
-router.delete('/:id',productController.deleteProduct);
-router.put('/:id/edit',productController.updateProductByID);
-router.get('/:id',productController.getProductByID);
-router.get('/',productController.getAllProduct);
+router.delete("/:id", productController.deleteProduct);
+router.put("/:id/", productController.updateProductByID);
+router.get("/:id", productController.getProductByID);
+router.post("/", productController.insertProduct);
+router.get("/", productController.getAllProduct);
 
 module.exports = router;
